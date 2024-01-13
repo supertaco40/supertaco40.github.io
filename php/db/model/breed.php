f358c493412ada204f16d9507d275814715197b8
supertaco40 <106879476+supertaco40@users.noreply.github.com>
2024-01-13 19:47:31 +0600

    Update breed.php

--- php/db/model/breed.php
+++ php/db/model/breed.php
@@ -5,11 +5,14 @@ class Breed
     public $id;
     public $name;
     public $description;
+    public $metadescription;
 
     function __construct($id, $name, $description, $metadescription, $keyword)
     {
         $this->id = $id;
         $this->name = $name;
         $this->description = $description;
+        $this->metadescription = $metadescription;
+        $this->$keyword = $keyword;
     }
 }
