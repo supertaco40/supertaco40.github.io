8a0b9eea217a801846d3667087ea738137248625
supertaco40 <106879476+supertaco40@users.noreply.github.com>
2024-01-13 21:55:50 +0600

    Update characteristic.php

--- php/db/model/characteristic.php
+++ php/db/model/characteristic.php
@@ -14,6 +14,6 @@ class Characteristic
         $this->name = $name;
         $this->description = $description;
         $this->metadescription = $metadescription;
-        $this->keyword = %keyword;
+        $this->keyword = $keyword;
     }
 }
