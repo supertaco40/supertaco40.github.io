513641c09eab0b8327a297114eca6513dbcb1719
supertaco40 <106879476+supertaco40@users.noreply.github.com>
2024-01-13 21:24:31 +0600

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,3 +1,3 @@
-export const base_url = 'https://proxy.cors.sh/http://31.184.253.173/php/cats'
+export const base_url = 'https://cors-anywhere.herokuapp.com/http://31.184.253.173/php/cats'
 
 export const site_url = 'https://supertaco40.github.io';
