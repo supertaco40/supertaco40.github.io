4ff736dc8849b561696658453a60dc735e092f20
supertaco40 <106879476+supertaco40@users.noreply.github.com>
2024-01-13 20:53:21 +0600

    Update characteristic.php

--- php/db/model/characteristic.php
+++ php/db/model/characteristic.php
@@ -5,11 +5,15 @@ class Characteristic
     public $id;
     public $name;
     public $description;
+    public $metadescription;
+    public $keyword;
 
-    function __construct($id, $name, $description)
+    function __construct($id, $name, $description, $metadescription, $keyword)
     {
         $this->id = $id;
         $this->name = $name;
         $this->description = $description;
+        $this->metadescription = $metadescription;
+        $this->keyword = %keyword;
     }
 }
